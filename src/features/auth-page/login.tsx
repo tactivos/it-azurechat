a231439464eb6c6163396c566216f95e5cad53c8
Mingus <megamingus@gmail.com>
2024-04-09 13:42:49 -0300

    Auto Sign-in and try to logout using Azure App Service authentication

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { AI_NAME } from "@/features/theme/theme-config";
 import { signIn } from "next-auth/react";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import {
@@ -17,6 +17,10 @@ interface LoginProps {
 }
 
 export const LogIn: FC<LoginProps> = (props) => {
+  useEffect(() => {
+    signIn("azure-ad");
+  });
+
   return (
     <Card className="flex gap-2 flex-col min-w-[300px]">
       <CardHeader className="gap-2">
@@ -27,18 +31,10 @@ export const LogIn: FC<LoginProps> = (props) => {
           <span className="text-primary">{AI_NAME}</span>
         </CardTitle>
         <CardDescription>
-          Login in with your GitHub or Microsoft 365 account
+          Signing in with your Microsoft account
         </CardDescription>
       </CardHeader>
-      <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
-        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
-        {props.isDevMode ? (
-          <Button onClick={() => signIn("localdev")}>
-            Basic Auth (DEV ONLY)
-          </Button>
-        ) : null}
-      </CardContent>
+      <CardContent className="grid gap-4">Signing in...</CardContent>
     </Card>
   );
 };
